cb9e35b46abbf152b545a5a8104c752626db2e89
spv420 <spv@spv.sh>
2022-07-13 00:12:39 -0400

    badges: wip rounded rectangle code

--- medusa_software/work/badges/main.py
+++ medusa_software/work/badges/main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
-import sys
+from PIL import Image, ImageDraw
+import sys, os
 
 def main(args):
+	if not (os.path.exists("pil")):
+		os.mkdir("pil")
+	img = Image.new('RGBA', (128, 32))
+	img.putalpha(256)
+	draw = ImageDraw.Draw(img)
+	draw.rounded_rectangle((0, 0, 128, 32), fill="#202020", outline="#f0f0f0", width=2, radius=8)
+	img.save('pil/pil_test.png')
 	print(args)
 
 if __name__ == "__main__":
