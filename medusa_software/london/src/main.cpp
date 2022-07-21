f8eec3605878d90b2c869459c3dd2b877099b7df
spv420 <spv@spv.sh>
2022-07-20 23:32:10 -0400

    london: add WIP source code

--- medusa_software/london/src/main.cpp
+++ medusa_software/london/src/main.cpp
@@ -15,9 +15,57 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include "medusa_window.h"
+#include "logging.h"
+#include <gtkmm.h>
 #include <cstdio>
 
-int main(int argc, char* argv[]) {
-	printf("Hello, world!\n");
-	return 0;
+int main(int   argc,
+		 char* argv[]) {
+	/*
+	 *  TODO: make this code look better, it looks like shit.
+	 *
+	 *  This code creates a fake argc and argv that only contains the name of
+	 *  the binary, otherwise GTK gets mad at the moment, complaining that we
+	 *  don't take files as arguments or something.
+	 *
+	 *  spv@jkek420:/.../xpdbg/xpdbg_software$ bin/main res/test1.bin
+	 *
+	 *  (main:13170): GLib-GIO-CRITICAL **: 14:22:18.291: This application can not open files.
+	 *  spv@jkek420:/.../xpdbg/xpdbg_software$
+	 */
+	int       fake_argc	= 1;
+	char**    fake_argv	= NULL;
+	fake_argv = (char**)calloc(sizeof(uintptr_t), 2);
+
+	/*
+	 *  populate the array
+	 */
+	fake_argv[0] = argv[0];
+	fake_argv[1] = NULL;
+
+	medusa_set_log_level(LOG_VERBOSE);
+	medusa_log(LOG_INFO, "Medusa Loaded.");
+
+	/*
+	 *  create the app
+	 */
+	medusa_log(LOG_INFO, "Creating GTK application...");
+	auto app = Gtk::Application::create(fake_argc,
+										fake_argv,
+										"org.medusa-re.medusa");
+
+	/*
+	 *  create the window object
+	 */
+	medusa_log(LOG_INFO, "Creating medusa_window...");
+	medusa_window window(argc,
+						 argv);
+
+
+	/*
+	 *  run it
+	 */
+	medusa_log(LOG_INFO, "Running medusa_window...");
+	return app->run(window);
 }
