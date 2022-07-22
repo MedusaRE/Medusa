27fe8cd7ac5ae38a0c7e1445c7fbb0de6b3e27db
spv420 <spv@spv.sh>
2022-07-22 16:58:50 -0400

    tools: update deps_arch.sh

--- tools/deps_arch.sh
+++ tools/deps_arch.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ $(id -u) != 0 ]; then
+    echo "error: this program must be run as root."
+    exit -1
+fi
+
 pacman -S git gtkmm3 gtk3 glade gimp texlive-most python3 python-pillow node npm
 npm install badgen
 
