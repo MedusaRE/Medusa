c6e24a91170f35efdfa7b6116b60e9ce12ea9169
spv420 <spv@spv.sh>
2022-08-01 04:17:37 -0400

    paris: socket code

--- medusa_software/paris/src/main.cpp
+++ medusa_software/paris/src/main.cpp
@@ -15,9 +15,38 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include <sys/socket.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <sys/un.h>
+#include <unistd.h>
 #include <cstdio>
 
 int main(int argc, char* argv[]) {
-	printf("Hello, world!\n");
+	struct sockaddr_un name;
+	size_t size;
+	int sock;
+
+	sock = socket(PF_LOCAL, SOCK_DGRAM, 0);
+
+	name.sun_family = AF_LOCAL;
+	strncpy(name.sun_path, argv[1], sizeof(name.sun_path));
+
+	size = ((offsetof(struct sockaddr_un, sun_path)
+			+ strlen(name.sun_path)));
+	
+	bind(sock, (struct sockaddr*)&name, size);
+
+	listen(sock, 1);
+	int data_sock = accept(sock, NULL, NULL);
+
+	while (1) {
+		char c[1];
+
+		read(data_sock, c, 1);
+
+		printf("%c", c[0]);
+	}
+
 	return 0;
 }
