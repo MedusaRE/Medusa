9456cff1b213ff1b6207333fa5202d62316a65ea
spv420 <spv@spv.sh>
2022-09-28 17:46:28 +0000

    ms/libmedusa;: more doxygen file headers

--- medusa_software/libmedusa/include/libmedusa/ARM64Machine.hpp
+++ medusa_software/libmedusa/include/libmedusa/ARM64Machine.hpp
@@ -1,18 +1,24 @@
-/*
- *  Copyright (C) 2022, w212 research. <contact@w212research.com>
- *
+/**
+ *  @file
+ *  @author  spv <spv@spv.sh>
+ * 
+ *  @section LICENSE
+ * 
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of version 2 of the GNU General Public License as
  *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details at
+ *  https://www.gnu.org/copyleft/gpl.html
+ * 
+ *  @section DESCRIPTION
+ * 
+ *  The ARM64Machine class implements the Machine generic class to interface
+ *  with an emulated generic ARM64 CPU, based on Unicorn, Capstone, and
+ *  Keystone.
  */
 
 #ifndef ARM64MACHINE_HPP
