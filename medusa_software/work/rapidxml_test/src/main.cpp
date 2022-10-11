4a3629bb990dae3709e5aaf47981909a17a6a9b1
spv420 <spv@spv.sh>
2022-10-11 01:25:52 -0400

    ms/work/rapidxml_test: more code

--- medusa_software/work/rapidxml_test/src/main.cpp
+++ medusa_software/work/rapidxml_test/src/main.cpp
@@ -15,9 +15,35 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include "../../../submodules/rapidxml/rapidxml.hpp"
+#include <fstream>
+#include <sstream>
 #include <cstdio>
+#include <string>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
 	printf("Hello, world!\n");
+
+	ifstream f("../../vienna/res/src/cpu_definitions/ARMv7.xml");
+	stringstream ss;
+
+	ss << f.rdbuf();
+
+	string s = ss.str();
+
+	rapidxml::xml_document<> doc;
+	doc.parse<0>((char*)s.c_str());
+
+
+	rapidxml::xml_node<>* node = doc.first_node()->first_node();
+	
+	do {
+		printf("%s\n", node->name());
+		node = node->next_sibling();
+	} while (node != NULL);
+
+
 	return 0;
 }
