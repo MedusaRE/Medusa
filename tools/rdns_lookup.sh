f6644cc914ccccdbe90ed72d9e75317c77ba21f3
spv420 <spv@spv.sh>
2022-10-15 17:35:40 -0400

    tools: update rdns_lookup.sh to support res/rdns.txt

--- tools/rdns_lookup.sh
+++ tools/rdns_lookup.sh
@@ -4,6 +4,12 @@ MEDUSA_PATH=$(dirname $(realpath $0))/..
 RDNS=$($MEDUSA_PATH/tools/rdns.sh $1)
 
 HOST_OUTPUT=$(host -t txt $RDNS)
+
+if [ $? -ne 0 ]; then
+	RDNS=$($MEDUSA_PATH/tools/rdns.sh $(cat $1/res/rdns.txt))
+	HOST_OUTPUT=$(host -t txt $RDNS)
+fi
+
 OUT_CUT=$(echo "$HOST_OUTPUT" | cut -d " " -f 4-)
 OUT_CHOPPED=$(echo $OUT_CUT | tail -c +2 | head -c -2)
 OUT_PROC=$(eval printf '%s\\n' "$OUT_CHOPPED")
