1eae9218636558fd9ca907731be5aeb18e3a911c
spv420 <spv@spv.sh>
2022-10-15 18:14:10 -0400

    tools: add more options to register_component_dns.sh

--- tools/register_component_rdns.sh
+++ tools/register_component_rdns.sh
@@ -2,4 +2,4 @@
 
 RDNS_OF_ARG1=$($MEDUSA_PATH/tools/rdns.sh $1)
 lexicon cloudflare delete $MEDUSA_DOMAIN TXT --name="$RDNS_OF_ARG1"
-lexicon cloudflare create $MEDUSA_DOMAIN TXT --name="$RDNS_OF_ARG1" --content="RDNS_NAME=\"$1\""
+lexicon cloudflare create $MEDUSA_DOMAIN TXT --name="$RDNS_OF_ARG1" --content="RDNS_NAME=\"$1\" PROJECT_DIR=\"$2\" PROJECT_DESC=\"$3\""
