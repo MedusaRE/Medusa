b0ec90fa4eda256eaad7308002961109d54f3822
spv420 <spv@spv.sh>
2022-10-15 17:38:30 -0400

    tools: add comments and support directly passing rdns.txt

--- tools/rdns_lookup.sh
+++ tools/rdns_lookup.sh
@@ -3,10 +3,21 @@
 MEDUSA_PATH=$(dirname $(realpath $0))/..
 RDNS=$($MEDUSA_PATH/tools/rdns.sh $1)
 
-HOST_OUTPUT=$(host -t txt $RDNS)
+# first, try the actual DNS we were passed (if it is actual DNS)
+HOST_OUTPUT=$(host -t txt $RDNS 2>/dev/null)
 
+# next, try reading the val we were passed + /res/rdns.txt, i.e.
+# medusa_software/vienna would read medusa_software/vienna/res/rdns.txt, the
+# "standard" path for the rDNS value
 if [ $? -ne 0 ]; then
-	RDNS=$($MEDUSA_PATH/tools/rdns.sh $(cat $1/res/rdns.txt))
+	RDNS=$($MEDUSA_PATH/tools/rdns.sh $(cat $1/res/rdns.txt 2>/dev/null) 2>/dev/null)
+	HOST_OUTPUT=$(host -t txt $RDNS 2>/dev/null)
+fi
+
+# finally, try just reading the val we were passed, like if someone passed the
+# path to rdns.txt directly
+if [ $? -ne 0 ]; then
+	RDNS=$($MEDUSA_PATH/tools/rdns.sh $(cat $1))
 	HOST_OUTPUT=$(host -t txt $RDNS)
 fi
 
