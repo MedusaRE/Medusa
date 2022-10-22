bb58d266ad5f14f8f81e1fa3f7c2dcd9f6ae30b5
spv420 <spv@spv.sh>
2022-10-22 19:11:40 -0400

    tools: add texlive back to deps_debian.sh, it's slow anyway

--- tools/deps_debian.sh
+++ tools/deps_debian.sh
@@ -6,7 +6,7 @@ if [ $(id -u) != 0 ]; then
 fi
 
 apt-get update
-apt-get -y install git libgtkmm-3.0-dev libgtk-3-dev glade gimp python3 python3-pillow nodejs npm doxygen clang-13
+apt-get -y install git libgtkmm-3.0-dev libgtk-3-dev glade gimp texlive python3 python3-pillow nodejs npm doxygen clang-13
 npm install badgen
 
 git submodule init
--- tools/deps_debian_non_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ $(id -u) != 0 ]; then
-    echo "error: this program must be run as root."
-    exit 1
-fi
-
-apt-get update
-apt-get -y install texlive
