8da57c385dedcca22ff2a1a13f86d4b796981f03
spv420 <spv@spv.sh>
2022-10-23 16:48:05 -0400

    ms/paris: remove even more unnecessary code

--- medusa_software/paris/src/main.cpp
+++ medusa_software/paris/src/main.cpp
@@ -28,72 +28,7 @@
 
 using namespace std;
 
-int connect_main(int argc, char* argv[]) {
-	struct sockaddr_un addr;
-	int data_socket = socket(AF_UNIX, SOCK_SEQPACKET, 0);
-
-	memset(&addr, 0, sizeof(addr));
-
-	addr.sun_family = AF_UNIX;
-	strncpy(addr.sun_path, argv[2], sizeof(addr.sun_path));
-
-	connect(data_socket, (const struct sockaddr*)&addr, sizeof(addr));
-
-	int n = strlen(argv[3]);
-	printf("%d\n", n);
-	for (int i = 0; i < n; i++) {
-		write(data_socket, &argv[3][i], 1);
-	}
-
-	close(data_socket);
-
-	return 0;
-}
-
 int main(int argc, char* argv[]) {
-#if 0
-	struct sockaddr_un name;
-	size_t size;
-	int sock;
-
-	if (strcmp(argv[1], "connect") == 0) {
-		return connect_main(argc, argv);
-	}
-
-	sock = socket(PF_LOCAL, SOCK_SEQPACKET, 0);
-	perror("lol");
-
-	name.sun_family = AF_LOCAL;
-	strncpy(name.sun_path, argv[1], sizeof(name.sun_path));
-
-	size = ((offsetof(struct sockaddr_un, sun_path)
-			+ strlen(name.sun_path)));
-	
-	bind(sock, (struct sockaddr*)&name, sizeof(name));
-	perror("lol");
-
-	listen(sock, 1);
-	perror("lol");
-
-	while (1) {
-		int data_socket = accept(sock, NULL, NULL);
-
-		while (1) {
-			char c[1];
-
-			int n = read(data_socket, c, 1);
-			if (n != 1) {
-				close(data_socket);
-				break;
-			}
-
-			printf("%c", c[0]);
-		}
-
-		printf("\n");
-	}
-#endif
-
 	DEBUG_PRINTF("creating Server object\n");
 
 	paris::Server server;
