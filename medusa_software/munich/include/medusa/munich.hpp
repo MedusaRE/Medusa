e352bd2546f625f37559b31eacc6d910083fdbf4
spv420 <spv@spv.sh>
2022-10-26 11:26:42 -0400

    ms/munich: add WIP code

---
+++ medusa_software/munich/include/medusa/munich.hpp
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (C) 2022, w212 research. <contact@w212research.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of version 2 of the GNU General Public License as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef MEDUSA_MUNICH_HPP
+#define MEDUSA_MUNICH_HPP
+
+#include <string>
+#include "../../src/lib.h"
+
+namespace munich {
+	class string : public std::string {
+		public:
+			template<typename... arg_type>
+			string(const char* s, arg_type... args) {
+				printf("%s\n", "abc");
+
+				this->assign(string_format_cstr(s, args...));
+			}
+	};
+}
+
+#endif
---
+++ medusa_software/munich/src/munich.cpp
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (C) 2022, w212 research. <contact@w212research.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of version 2 of the GNU General Public License as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <medusa/munich.hpp>
+#include <cstdio>
+#include "lib.h"
+
+#if 0
+using namespace munich;
+
+template<typename... arg_type>
+string::string(const char* s, arg_type... args) {
+	printf("%s\n", "abc");
+
+	this->assign(string_format_cstr(s, args...));
+}
+#endif
