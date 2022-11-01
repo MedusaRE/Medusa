9bb0d08fd0ed82a5b25aa1c1e722cf606cc70dad
spv420 <spv@spv.sh>
2022-11-01 11:13:37 -0400

    ms/frontend: hopefully pass CI

---
+++ medusa_software/frontend/src/medusa_application.cpp
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (C) 2022, w212 research. <contact@w212research.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of version 2 of the GNU General Public License as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "medusa_application.h"
+#include "medusa_window.h"
+#include <gtkmm.h>
+
+medusa_application::medusa_application() : Gtk::Application("org.medusa-re.medusa.frontend", Gio::Application::Flags::HANDLES_OPEN) {
+	//
+}
+
+Glib::RefPtr<medusa_application> medusa_application::create() {
+	return Glib::make_refptr_for_instance<medusa_application>(new medusa_application());
+}
+
+medusa_window* medusa_application::create_appwindow() {
+	auto appwindow = new medusa_window();
+
+	add_window(*appwindow);
+
+	return appwindow;
+}
---
+++ medusa_software/frontend/src/medusa_application.h
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (C) 2022, w212 research. <contact@w212research.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of version 2 of the GNU General Public License as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef MEDUSA_WINDOW_H
+#define MEDUSA_WINDOW_H
+
+#include <gtkmm.h>
+
+class medusa_application : public Gtk::Application {
+	public:
+		medusa_application(int   argc,
+						   char* argv[]);
+		static Glib::RefPtr<ExampleApplication> create();
+	protected:
+		Gtk::Button step_button;
+		Gtk::ScrolledWindow sw;
+		Gtk::TextView reg_view;
+		Gtk::Box button_box;
+		Gtk::Grid our_grid;
+		Gtk::Box emu_box;
+		Gtk::Box our_box;
+	
+		void step_clicked();
+};
+
+#endif
