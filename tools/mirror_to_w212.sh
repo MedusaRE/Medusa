208c3b90f8ab35d9f9281f5162191142b90d8e69
spv420 <spv@spv.sh>
2023-02-23 12:14:28 -0500

    tools: add mirror_to_w212.sh

---
+++ tools/mirror_to_w212.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#  Copyright (C) 2023, w212 research. <contact@w212research.com>
+#
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of version 2 of the GNU General Public License as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+if [ -z "${MEDUSA_PATH}" ]; then
+    source $(realpath $(dirname $0))/source_env.sh
+fi
+
+cd $MEDUSA_PATH
+
+git push --mirror https://git.w212.org/Medusa.git
